9a8ece13f2a4273511f5a2c01071f1c5ffb2094c
Josef Zacek <info@josefzacek.com>
2016-05-17 22:17:01 +0100

    create routes for :places, only: [:index, :new, :create]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :places, only: [:index, :new, :create]
+
   root 'places#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
