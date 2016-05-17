4f873136d97a9c94c1a808bdf0674dcf06f4e1f4
Josef Zacek <info@josefzacek.com>
2016-05-17 22:15:13 +0100

    run annotate command

---
+++ app/models/place.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  address     :string
+#  latitude    :float
+#  longitude   :float
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
