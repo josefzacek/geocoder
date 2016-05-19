8d44d3851fb17ec923d55d59ef143dfde894657f
Josef Zacek <info@josefzacek.com>
2016-05-19 23:52:55 +0100

    added show, new, create methods + strong params

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,27 @@ class PlacesController < ApplicationController
     @places = Place.order('created_at DESC')
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
   def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      flash[:success] = 'New place added'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:title, :description, :address)
   end
 end
