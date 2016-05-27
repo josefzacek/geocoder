1a00cbf1978ef416172f23543a74ae2305592844
Josef Zacek <info@josefzacek.com>
2016-05-27 22:47:24 +0100

    run annotate

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  address     :string
+#  city        :string
+#  country     :string
+#  county      :string
+#  postal_code :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  latitude    :float
+#  longitude   :float
+#
+
 class Location < ActiveRecord::Base
 end
